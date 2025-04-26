c31a69131157a3d9125811e43081a74c48f1fc52
Konstantin <korchikov_k@mail.ru>
2025-04-26 11:43:32 +0300

    feat: add burger menu

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const burgerBtn = document.querySelector('.burger'),
+  menu = document.querySelector('.menu'),
+  closeBtn = document.querySelector('.menu__close'),
+  overlay = document.querySelector('.menu__overlay');
+
+burgerBtn.addEventListener('click', () => {
+  menu.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
